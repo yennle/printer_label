f24954533291d562f6c4a3cb754f12d8044c2a5f
renratwit <59679274+renratwit@users.noreply.github.com>
2021-01-15 11:05:28 -0500

    Update xlm-mod to include objects

--- xlm-mod.py
+++ xlm-mod.py
@@ -1,6 +1,23 @@
 from xml.dom import minidom
 
-datas = ["Part", "Rev", "Date", "PO"]
+class Data:
+    def __init__(self, name, value, font_h, x, y):
+        self.name = name
+        self.value = value
+        self.font_h = font_h
+        self.x = x
+        self.y = y
+
+f_height = "10.000"
+
+part = Data("Part #", "CN-321", "14.000", "0.080", "0.000")
+rev = Data("Rev", "31", f_height, "0.000", "0.300")
+dash_1 = Data("dash1", "-", f_height, "0.150", "0.350")
+date = Data("Date", "2101", f_height, "0.250", "0.300")
+dash_2 = Data("dash2", "-", f_height, "0.580", "0.350")
+po = Data("PO", "12345678", f_height, "0.700", "0.300")
+
+datas = [part, rev, dash_1, date, po, dash_2]
 
 DOMimpl = minidom.getDOMImplementation()
 
@@ -53,7 +70,7 @@ image.appendChild(fields)
 for d in datas:
     #Create Textfield
     textfield = xmldoc.createElement("Textfield")
-    textfield.setAttribute("id", d)
+    textfield.setAttribute("id", d.name)
 
     #Volatile
     volatile = xmldoc.createElement("Volatile")
@@ -63,13 +80,13 @@ for d in datas:
     #BoundingBox
     boundingbox = xmldoc.createElement("BoundingBox")
     boundingbox.setAttribute("units", "Inches")
-    boundingbox.setAttribute("x", "0.080")
-    boundingbox.setAttribute("y", "0.000")
+    boundingbox.setAttribute("x", d.x)
+    boundingbox.setAttribute("y", d.y)
     textfield.appendChild(boundingbox)
 
     #<Font pointsizeheight="12.000" pointsizewidth="0.000" italics="0" weight="400">Arial Bold</Font>
     font = xmldoc.createElement("Font")
-    font.setAttribute("pointsizeheight", "12.000")
+    font.setAttribute("pointsizeheight", d.font_h)
     font.setAttribute("pointsizewidth", "0.000")
     font.setAttribute("italics", "0")
     font.setAttribute("weight", "400")
@@ -98,7 +115,7 @@ for d in datas:
 
     #create Data
     element = xmldoc.createElement("Data")
-    element.appendChild(xmldoc.createTextNode(d))
+    element.appendChild(xmldoc.createTextNode(d.value))
     textfield.appendChild(element)
 
     #append to textfield
